b24a636f7a7f42d5449c1c3c70f9b5edb8ef18c4
Malathi416 <ammu.416@gmail.com>
2019-11-26 15:06:52 -0600

    Completed the code for all Classes up to Part 3

--- src/org/launchcode/techjobs_oo/CoreCompetency.java
+++ src/org/launchcode/techjobs_oo/CoreCompetency.java
@@ -24,6 +24,8 @@ public class CoreCompetency {
         return value;
     }
 
+
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -39,4 +41,16 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
